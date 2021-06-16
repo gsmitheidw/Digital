6c54929fc8831a385da485172f07e93ccc96a72d
Graham Smith <gsmitheidw@gmail.com>
2021-06-16 19:25:32 +0100

    package created

---
+++ Digital/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/hneemann/Digital/releases/latest/download/Digital.zip' 
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'Digital*' 
+  checksum      = 'A93B9DD70ED5B051BF04AACC37F2794C3C3D17F214AB99F0C8F4A36E55BBE835'
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$target = Join-Path $toolsDir "$($packageName).exe"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Digital.lnk" -targetPath $target 
