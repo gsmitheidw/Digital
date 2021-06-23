1eadd7df89877a045ed04367266284c715dbfcbd
Graham Smith <gsmitheidw@gmail.com>
2021-06-23 11:03:26 +0100

    better java runtime checking

--- Digital/tools/chocolateyinstall.ps1
+++ Digital/tools/chocolateyinstall.ps1
@@ -17,5 +17,11 @@ $target = Join-Path $toolsDir "Digital\$($packageName).exe"
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Digital.lnk" -targetPath $target 
 
 # We cannot set a specific dependency for Java Runtime as Oracle JRE, JDK and OpenJDK etc are all possible alternatives
-# In light of this, warn the user:
-Write-Warning "Note: a valid Java Runtime is required such as jre or equivavlent"
+# In light of this, check java.exe is on the path and warn the user if none found:
+
+[bool]$javafound = (Get-Command java -ErrorAction SilentlyContinue).Name -eq 'java.exe'
+	
+	if ($javafound = $false) 
+	{
+	Write-Warning "Note: No Java Java Runtime found, this is required"
+	}
